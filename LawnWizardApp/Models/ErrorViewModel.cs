f6692ac0d4c7f3e556fb75e8ce42b684dd9d06db
CSHepworth <109162129+CSHepworth@users.noreply.github.com>
2023-04-20 09:49:59 -0600

    Removed old lawn wizard app

--- LawnWizardApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LawnWizardApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
