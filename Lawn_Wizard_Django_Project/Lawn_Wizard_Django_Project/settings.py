18a0aa7d41edca2a8556d24239613921d42d0a71
CSHepworth <sircooperhepworth@gmail.com>
2025-07-10 20:20:15 -0600

    created register customer app in Lawn Wizard Django Project

--- Lawn_Wizard_Django_Project/Lawn_Wizard_Django_Project/settings.py
+++ Lawn_Wizard_Django_Project/Lawn_Wizard_Django_Project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "register_customer",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
---
+++ Lawn_Wizard_Django_Project/register_customer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RegisterCustomerConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "register_customer"
