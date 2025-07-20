b4421a48977a46e59eed93b66fb3277ae850af15
CSHepworth <sircooperhepworth@gmail.com>
2025-07-19 18:22:40 -0600

    added foreign key to employee model, removed descriptive text

--- Lawn_Wizard_Django_Project/address_manager/models.py
+++ Lawn_Wizard_Django_Project/address_manager/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Address(models.Model):
     id = models.AutoField(primary_key = True)
     address_line_1 = models.CharField(max_length = 100)
--- Lawn_Wizard_Django_Project/employees/models.py
+++ Lawn_Wizard_Django_Project/employees/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from address_manager import Address
 
-# Create your models here.
 class Employee(models.Model):
     id = models.AutoField(primary_key = True)
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length = 100)
+    address = models.ForeignKey(Address)
     dob = models.DateField(null = True)
     hire_date = models.DateField()
     employment_end_date = models.DateField(null = True)
